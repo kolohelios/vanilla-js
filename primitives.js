c3f0cf8398a7914b37e0e4937d2b2b5ddea799c5
Jon Edwards <jkedwards@me.com>
2015-04-11 10:38:37 -0700

    primitives

---
+++ primitives.js
@@ -0,0 +1,31 @@
+// this is a single line comment
+
+/* this
+ * is
+  * multi-line comment
+  */
+
+// 5 primitive types of variables
+// string, number, boolean, null, undefined
+
+// srings have either single or double quotes
+var a = 'hello';
+var b = 'world';
+console.log(a, b);
+
+// numbers are either integer or floating point
+var c = 3;
+var d = 3.14;
+console.log(c, d);
+
+// boolean are either true or false
+var e = true;
+var f = false;
+var g = 'a' === 'b';
+console.log(e, f, g);
+
+// null and undefined
+var h = null;
+console.log(h);
+var i;
+console.log(i);
